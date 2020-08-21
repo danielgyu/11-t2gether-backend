75852b85623fadcf91650ec7471c9bf6ef1dc271
Minwook11 <alsdnr4874@gmail.com>
2020-08-21 13:30:45 +0900

    Refactor: 리뷰 반영 - Hash 암호화 알고리즘 종류 별도 파일로 관리하도록 수정 - 로그인 진행 시 입력받은 이메일이 DB에 존재하는지 확인하는 코드 추가 - 비밀번호 유효성 검사에 정규식을 사용하는 방법으로 수정

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 import json
 import bcrypt
 import jwt
+import re
 
 from django.views import View
 from django.http  import JsonResponse
@@ -9,18 +10,13 @@ from .models import User
 import my_setting
 
 def pw_validate(pw):
-    large_case = False
-    number     = False
     if len(pw) < 8:
         return False
-    for each_item in pw:
-        if each_item.isupper():
-            large_case = True
-        if each_item.isdigit():
-            number = True
-    if large_case and number:
-        return True
-    return False
+    is_largeCase = re.compile('[A-Z]')
+    is_numeric = re.compile('[0-9]')
+    if not(is_largeCase.search(pw)) or not(is_numeric.search(pw)):
+        return False
+    return True
 
 class JoinView(View):
     def post(self, request):
@@ -35,16 +31,20 @@ class JoinView(View):
             input_news  = data['is_newsletter_subscribed']
 
             if ('@' not in input_email) or ('.' not in input_email):
-               return JsonResponse({'message' : 'EMAIL_VALIDATION_FAILED'}, status = 400)
+                return JsonResponse({'message' : 'WRONG_EMAIL_FORMAT'}, status = 400)
             if not(pw_validate(input_pw)):
-                return JsonResponse({'message' : 'PASSWORD_CHECK_FAILED'}, status = 400)
+                return JsonResponse({'message' : 'WRONG_PASSWORD_CONDITION'}, status = 400)
+            if User.objects.filter(email = input_email).exists():
+                return JsonResponse({'message' : 'ALREADY_USE_EMAIL'}, status = 400)
+            if User.objects.filter(phone = input_phone).exists():
+                return JsonResponse({'message' : 'ALREADY_USE_PHONE_NUMBER'}, status = 400)
 
             User(
                 first_name               = input_first,
                 last_name                = input_last,
                 email                    = input_email,
                 phone                    = input_phone,
-                password                 = (bcrypt.hashpw(input_pw.encode('utf-8'), bcrypt.gensalt())).decode('utf-8'),
+                password                 = (bcrypt.hashpw(input_pw.encode(my_setting.ENCODING_FORMAT), bcrypt.gensalt())).decode(my_setting.ENCODING_FORMAT),
                 birthdate                = input_birth,
                 is_newsletter_subscribed = input_news,
                 is_top_contributor       = False
@@ -53,9 +53,6 @@ class JoinView(View):
             return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
         return JsonResponse({'message' : 'SUCCESS'}, status = 200)
 
-    def get(self, request):
-        return JsonResponse({'message' : 'HTTP GET'}, status = 200)
-
 class LogInView(View):
     def post(self, request):
         try:
@@ -63,10 +60,13 @@ class LogInView(View):
             input_email = data['email']
             input_pw    = data['password']
 
+            if not(User.objects.filter(email = input_email).exists()):
+                return JsonResponse({'message' : 'WRONG_LOGIN_INFORMATION'}, status = 400)
+
             saved_password = User.objects.get(email = input_email).password
-            if bcrypt.checkpw(input_pw.encode('utf-8'), saved_password.encode('utf-8')):
-                login_token = jwt.encode({'user_id' : User.objects.get(password = saved_password).id}, my_setting.SECRET_KEY, algorithm = 'HS256')
-                return JsonResponse({'message' : login_token.decode('utf-8')}, status = 200)
+            if bcrypt.checkpw(input_pw.encode(my_setting.ENCODING_FORMAT), saved_password.encode(my_setting.ENCODING_FORMAT)):
+                login_token = jwt.encode({'user_id' : User.objects.get(password = saved_password).id}, my_setting.SECRET_KEY, algorithm = my_setting.HASH_ALGORITHM)
+                return JsonResponse({'message' : login_token.decode(my_setting.ENCODING_FORMAT)}, status = 200)
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
-        return JsonResponse({'message' : 'LOGIN_FAILED'}, status = 400)
+        return JsonResponse({'message' : 'WRONG_LOGIN_INFORMATION'}, status = 400)
