c77b6fab0347e12b78efb1f1d255be7ef9fd5bec
kungyu <hismayfly@gmail.com>
2020-08-24 18:39:49 +0900

    Fix : 위코드 컨벤션으로 수정

--- product/views.py
+++ product/views.py
@@ -1,22 +1,21 @@
-from django.views            import View
-from django.http             import JsonResponse
+from django.views import View
+from django.http  import JsonResponse
 
-from .models     import Product, Size
+from .models      import Product, Size
 
 
 class AllTeaView(View):
-    @query_debugger
     def get(self, request):
         tea_products = Product.objects.prefetch_related('size_set')
 
         tea_list = [{
-            'product_id' : product.id,
-            'product_name' : product.main_name,
+            'product_id'    : product.id,
+            'product_name'  : product.main_name,
             'product_price' : product.main_price,
             'product_image' : product.main_image,
-            'size_unit' : [tea.unit for tea in product.size_set.all()],
-            'size_price' : [tea.price for tea in product.size_set.all()],
-            'size_image' : [tea.image for tea in product.size_set.all()],
+            'size_unit'     : [tea.unit for tea in product.size_set.all()],
+            'size_price'    : [tea.price for tea in product.size_set.all()],
+            'size_image'    : [tea.image for tea in product.size_set.all()],
         } for product in tea_products]
 
         return JsonResponse({'product_list' : tea_list[:30]}, status = 200)
