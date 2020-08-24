129e1296893e78d8de83e0e7d7e982309486e02e
Minwook11 <alsdnr4874@gmail.com>
2020-08-24 13:13:21 +0900

    Modify: user 추가 기능을 위한 Model 수정 - 위시리스트, 장바구니 기능 구현을 위한 테이블 추가

--- user/models.py
+++ user/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from product.models import Product
 
 class User(models.Model):
     first_name               = models.CharField(max_length = 64, null = False)
@@ -9,3 +10,14 @@ class User(models.Model):
     birthdate                = models.DateField(null = True)
     is_newsletter_subscribed = models.BooleanField()
     is_top_contributor       = models.BooleanField()
+    wishlist                 = models.ManyToManyField('Product', thourgh = 'Wishlist')
+    shoppingbag              = models.ManyToManyField('Product', through = 'ShoppingBag')
+
+class Wishlist(models.Model):
+    user_id    = models.ForeignKey('User', on_delete = models.CASCADE)
+    product_id = models.ForeignKey('Product', on_delete = models.CASCADE)
+
+class ShoppingBag(models.Model):
+    user_id   = models.ForeignKey('User', on_delete = models.CASCADE)
+    procut_id = models.ForeignKey('Product', on_delete)
+    count     = models.IntegerField()
