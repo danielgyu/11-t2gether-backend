48d5e29aa2b1bfbd6bfaf0e18def36865506ae53
Minwook11 <alsdnr4874@gmail.com>
2020-08-24 17:08:14 +0900

    Fix: user models.py 컨벤션에 맞게 수정 - 코드 재정렬

--- user/models.py
+++ user/models.py
@@ -10,14 +10,14 @@ class User(models.Model):
     birthdate                = models.DateField(null = True)
     is_newsletter_subscribed = models.BooleanField()
     is_top_contributor       = models.BooleanField()
-    wish_list                 = models.ManyToManyField(Product, through = 'Wishlist', related_name = 'wish')
-    shopping_bag              = models.ManyToManyField(Product, through = 'ShoppingBag', related_name='shopping')
+    wish_list                = models.ManyToManyField(Product, through = 'Wishlist', related_name = 'wish')
+    shopping_bag             = models.ManyToManyField(Product, through = 'ShoppingBag', related_name = 'shopping')
 
 class Wishlist(models.Model):
     user_id    = models.ForeignKey(User, on_delete = models.CASCADE)
     product_id = models.ForeignKey(Product, on_delete = models.CASCADE)
 
 class ShoppingBag(models.Model):
-    user_id   = models.ForeignKey(User, on_delete = models.CASCADE)
-    procut_id = models.ForeignKey(Product, on_delete = models.CASCADE)
-    count     = models.IntegerField()
+    user_id    = models.ForeignKey(User, on_delete = models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete = models.CASCADE)
+    count      = models.IntegerField()
