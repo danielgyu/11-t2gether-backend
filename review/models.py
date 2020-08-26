e4bafb267efc9886257e3cbedc4d1296f3a7db65
kungyu <hismayfly@gmail.com>
2020-08-26 10:31:58 +0900

    Add: Review app models.py

---
+++ review/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class Review(models.Model):
+    rating  = models.DecimalField(max_digits = 3, decimal_places = 2, null = False)
+    product = models.ForeignKey('product.Product', on_delete = models.CASCADE)
