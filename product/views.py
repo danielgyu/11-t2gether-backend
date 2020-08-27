cdad11ed8b41fe4649b3bfc492b986e6e0088c0f
kungyu <hismayfly@gmail.com>
2020-08-27 13:09:55 +0900

    Add & Rfactor: AllTeaView및 RefineView 리팩토링 & search 추가 - elasticsearch를 통한 search(querystring) 기능추가

--- product/views.py
+++ product/views.py
@@ -4,17 +4,21 @@ from django.views     import View
 from django.http      import JsonResponse
 from django.db.models import Q
 
+from elasticsearch_dsl.connections import connections
+
 from .models      import Product, Size, Filter
+from .documents   import ProductDocument
 
 
 class AllTeaView(View):
     def get(self, request):
         styles = request.GET.getlist('style', None)
         types = request.GET.getlist('type', None)
+        search_term  = request.GET.get('search', None)
         tea_products = Product.objects.prefetch_related('size_set', 'refine_set')
 
         if styles and types:
-            tea_products = Product.objects.filter(refine__name__in = styles).filter(refine__name__in = types)
+            tea_products = tea_products.filter(refine__name__in = styles).filter(refine__name__in = types)
         else:
             q = Q()
             if styles:
@@ -22,6 +26,11 @@ class AllTeaView(View):
             if types:
                 q &= Q(refine__name__in = types)
             tea_products = tea_products.filter(q)
+
+        if search_term:
+            search = ProductDocument.search().filter('match_phrase', main_name = search_term)
+            search = [hit.main_name for hit in search]
+            tea_products = tea_products.filter(main_name__in = search)
 
         tea_list = [{
             'product_id'    : product.id,
@@ -37,20 +46,17 @@ class AllTeaView(View):
 
 class RefineView(View):
     def get(self, request):
-        styles   = request.GET.getlist('style', None)
-        teas     = request.GET.getlist('type', None)
-        tea_products = []
+        data     = dict(request.GET.items())
+        tea_products = Filter.objects.prefetch_related('product', 'refine')
 
-        if styles:
-            tea_products += list(Filter.objects.filter(
-                refine__name__in = styles).values(
-                    'product__refine__name', 'product__refine__category').distinct())
-        if teas:
-            tea_products += list(Filter.objects.filter(
-                refine__name__in = teas).values(
-                    'product__refine__name', 'product__refine__category').distinct())
-        if not styles and not teas:
-            tea_products = list(Filter.objects.values('refine__category', 'refine__name').distinct())
+        q = Q()
+        if data:
+            tea_products = \
+                    list(tea_products.filter(refine__name__in = list(data.values()))\
+                         .values('product__refine__name', 'product__refine__category').distinct())
+
+        if not data:
+            tea_products = list(tea_products.values('refine__category', 'refine__name').distinct())
 
         return JsonResponse({'product_list' : tea_products}, status = 200)
 
