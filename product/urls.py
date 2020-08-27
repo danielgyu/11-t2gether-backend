bdc9b92fcc4e3202902d67ba2c9503351a6ee5e8
kungyu <hismayfly@gmail.com>
2020-08-27 17:09:00 +0900

    Delete urls.py

--- product/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import AllTeaView, RefineView, TeaDetailView
-
-urlpatterns = [
-    path('', AllTeaView.as_view()),
-    path('/<int:id>', TeaDetailView.as_view()),
-    path('/refine', RefineView.as_view()),
-]
