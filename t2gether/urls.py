91d302533a88fe827e68b1fe4a176a3aaee419e2
Ubuntu <ubuntu@ip-172-31-31-196.us-east-2.compute.internal>
2020-08-30 12:31:34 +0000

    Modify: 20200828 시연기준 서버 - 생년월일 미처리

--- t2gether/urls.py
+++ t2gether/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('user', include('user.urls')),
     path('products', include('product.urls')),
     path('reviews', include('review.urls')),
+    path('main', include('main.urls')),
 ]
--- user/views.py
+++ user/views.py
@@ -2,13 +2,14 @@ import json
 import bcrypt
 import jwt
 import re
+import datetime
 
 from django.views import View
 from django.http  import JsonResponse
 
-from .models import User, Wishlist, ShoppingBag
+from .models        import User, Wishlist, ShoppingBag
 from product.models import Product, Size
-from .utils import login_decorator
+from .utils         import login_decorator
 import my_setting
 
 def pw_validate(pw):
@@ -24,8 +25,13 @@ class JoinView(View):
             input_email = data['email']
             input_pw    = data['password']
             input_phone = data['phone']
-            input_birth = data['birthdate']
-            input_news  = data['is_newsletter_subscribed']
+            input_year  = "2020"
+            input_month = "08"
+            input_day   = "28"
+#            input_news  = data['is_newsletter_subscribed']
+
+            temp_birth = input_year + '-' + input_month + '-' + input_day
+            input_birth = datetime.datetime.strptime(temp_birth, "%Y-%m-%d").date()
 
             is_email_format = re.compile('[@]((\.)|(([\w-]+\.)+))')
             if not(is_email_format.search(input_email)):
@@ -44,7 +50,7 @@ class JoinView(View):
                 phone      = input_phone,
                 password   = (bcrypt.hashpw(input_pw.encode(my_setting.ENCODING_FORMAT), bcrypt.gensalt())).decode(my_setting.ENCODING_FORMAT),
                 birthdate  = input_birth,
-                is_newsletter_subscribed = input_news,
+                is_newsletter_subscribed = False,
                 is_top_contributor       = False
             ).save()
         except KeyError:
@@ -105,7 +111,7 @@ class WishlistView(View):
                 Wishlist.objects.filter(user_id = user, product_id = target_product_id).delete()
                 res_dict['product_id'] = input_product_id
                 res_dict['is_wished'] = False
-                return JsonResponse({'message':res_dict}, status = 204)
+                return JsonResponse({'message':res_dict}, status = 201)
 
             Wishlist(
                 user_id = user,
@@ -135,34 +141,59 @@ class WishlistView(View):
 class ShoppingBagView(View):
     @login_decorator
     def post(self, request):
-        data              = json.loads(request.body)
+        data             = json.loads(request.body)
         input_user       = request.user
         input_product_id = data['product_id']
-        input_size        = data['size_unit']
-        input_count       = data['count']
+        input_size       = data['size_unit']
+        input_count      = data['count']
 
         if ShoppingBag.objects.filter(user_id = input_user, product_id = input_product_id, size = input_size).exists():
             target_instance = ShoppingBag.objects.filter(user_id = input_user, product_id = input_product_id, size = input_size).get()
             target_instance.count = target_instance.count + int(input_count)
             target_instance.save()
-            return JsonResponse({'message':target_instance.count}, status = 200)
-        input_product = Product.objects.get(id = input_product_id)
-        ShoppingBag(
-            user_id = input_user,
-            product_id = input_product,
-            size = input_size,
-            count = input_count
-        ).save()
+        else:
+            input_product = Product.objects.get(id = input_product_id)
+            ShoppingBag(
+                user_id    = input_user,
+                product_id = input_product,
+                size       = input_size,
+                count      = input_count
+            ).save()
 
-        return JsonResponse({'message':'SUCCESS'}, status = 200)
+        shopping_products = ShoppingBag.objects.filter(user_id = input_user).select_related('product_id')
+        size_sets = Size.objects.all()
+        size_checker = lambda target: 1 if target == '-1' else 0
+
+        user_shopping_bag = []
+        shopping_bag_ordering = 1
+        for each_item in shopping_products:
+            temp_dict = {}
+            temp_dict['ordering_number'] = shopping_bag_ordering
+            shopping_bag_ordering += 1
+            if size_checker(each_item.size):
+                temp_dict['name']  = each_item.product_id.main_name
+                temp_dict['image'] = each_item.product_id.main_image
+                temp_dict['count'] = each_item.count
+                temp_dict['price'] = (each_item.product_id.main_price) * (each_item.count)
+                user_shopping_bag.append(temp_dict)
+            if not(size_checker(each_item.size)):
+                target_size = size_sets.get(product_id = each_item.product_id.id, unit = each_item.size)
+                temp_dict['name']  = each_item.product_id.main_name
+                temp_dict['image'] = target_size.image
+                temp_dict['unit']  = target_size.unit
+                temp_dict['count'] = each_item.count
+                temp_dict['price'] = (target_size.price) * (each_item.count)
+                user_shopping_bag.append(temp_dict)
+        return JsonResponse({'message': user_shopping_bag}, status = 200)
 
     @login_decorator
     def get(self, request):
         user                = request.user
-        target_id   = request.GET.get('product_id', None)
+        target_name   = request.GET.get('product_name', None)
         target_size = request.GET.get('product_size', None)
 
-        if target_id and target_size:
+        if target_name and target_size:
+            target_id = Product.objects.get(main_name = target_name)
             if ShoppingBag.objects.filter(user_id = user, product_id = target_id, size = target_size).exists():
                 ShoppingBag.objects.get(user_id = user, product_id = target_id, size = target_size).delete()
 
@@ -171,8 +202,11 @@ class ShoppingBagView(View):
         size_checker = lambda target: 1 if target == '-1' else 0
 
         user_shopping_bag = []
+        shopping_bag_ordering = 1
         for each_item in shopping_products:
             temp_dict = {}
+            temp_dict['ordering_number'] = shopping_bag_ordering
+            shopping_bag_ordering += 1
             if size_checker(each_item.size):
                 temp_dict['name']  = each_item.product_id.main_name
                 temp_dict['image'] = each_item.product_id.main_image
